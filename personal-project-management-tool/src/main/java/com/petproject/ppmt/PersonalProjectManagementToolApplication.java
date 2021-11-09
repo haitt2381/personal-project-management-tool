33b916b6787066b8221ca0c64621c6365563e348
HaiTT <tranhai01se@gmail.com>
2021-11-09 22:10:48 +0700

    initial project

---
+++ personal-project-management-tool/src/main/java/com/petproject/ppmt/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.ppmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+    }
+
+}
