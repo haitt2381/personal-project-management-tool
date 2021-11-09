ce83697b77dbff995e8c03d351b916a265e3809d
HaiTT <tranhai01se@gmail.com>
2021-11-09 23:16:16 +0700

    Update project object, add repository

--- personal-project-management-tool/src/main/java/com/petproject/ppmt/domain/Project.java
+++ personal-project-management-tool/src/main/java/com/petproject/ppmt/domain/Project.java
@@ -1,4 +1,34 @@
 package com.petproject.ppmt.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = new Date();
+    }
 }
