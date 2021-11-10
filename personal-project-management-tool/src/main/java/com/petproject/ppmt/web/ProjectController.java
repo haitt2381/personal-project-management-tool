a0a379776292963a81f1b300329a365cd04f2a67
HaiTT <tranhai01se@gmail.com>
2021-11-10 12:34:05 +0700

    Update project object validation

--- personal-project-management-tool/src/main/java/com/petproject/ppmt/web/ProjectController.java
+++ personal-project-management-tool/src/main/java/com/petproject/ppmt/web/ProjectController.java
@@ -5,10 +5,16 @@ import com.petproject.ppmt.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +23,18 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error : result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
          Project projectRes = projectService.saveOrUpdate(project);
         return new ResponseEntity<>(projectRes, HttpStatus.CREATED);
     }
