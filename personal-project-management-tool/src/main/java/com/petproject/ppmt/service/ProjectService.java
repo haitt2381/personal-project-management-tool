04c1bcb683c48e96fa2161762931c8267a7866e6
HaiTT <tranhai01se@gmail.com>
2021-11-11 15:35:17 +0700

    Add exception handler duplicate project id

--- personal-project-management-tool/src/main/java/com/petproject/ppmt/service/ProjectService.java
+++ personal-project-management-tool/src/main/java/com/petproject/ppmt/service/ProjectService.java
@@ -1,8 +1,10 @@
 package com.petproject.ppmt.service;
 
 import com.petproject.ppmt.domain.Project;
+import com.petproject.ppmt.exception.ProjectIdException;
 import com.petproject.ppmt.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,7 +13,11 @@ public class ProjectService {
     private final ProjectRepository projectRepo;
 
     public Project saveOrUpdate(Project project){
-
-        return projectRepo.save(project);
+        project.setIdentifier(project.getIdentifier().toUpperCase());
+        try{
+            return projectRepo.save(project);
+        }catch (Exception ex){
+            throw new ProjectIdException("Project ID: '"+project.getIdentifier()+"' already exits");
+        }
     }
 }
