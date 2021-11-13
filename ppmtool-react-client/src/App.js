e73ad8479fc90cfd233403521eb69804bb175173
HaiTT <tranhai01se@gmail.com>
2021-11-13 18:18:06 +0700

    Init dashboard component

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import DashBoard from './component/Dashboard.js'
+
+function App() {
+  return <DashBoard />
+}
+
+export default App
---
+++ ppmtool-react-client/src/component/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DashBoard extends Component {
+    render() {
+        return (
+            <h1>
+                Welcome to the Dashboard
+            </h1>
+        );
+    }
+}
+
+export default DashBoard;
